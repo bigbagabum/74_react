559c56a0077e9023ef7d5b2e5253eaa88d85cee2
Dmitrii Evdokimov <evdokimovbest@gmail.com>
2026-01-06 13:03:19 +0100

    add project

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function Button({ name, onButtonClick }) {
+  return (
+    <button className="button" onClick={onButtonClick}>
+      {name}
+    </button>
+  );
+}
+
+export default Button;
